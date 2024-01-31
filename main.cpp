cdd2a2fbd29060b36eba23dbc89802d93a008fb0
kodai <kodai@MSI>
2024-01-31 11:24:19 +0900

    タイトルバー修正(完成)

--- main.cpp
+++ main.cpp
@@ -407,7 +407,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,      //　利用するクラス名  
-		L"CG2",                //　タイトルバーの文字
+		L"CG3_評価課題",                //　タイトルバーの文字
 		WS_OVERLAPPEDWINDOW,   //　よく見るウィンドウスタイル
 		CW_USEDEFAULT,         //　表示X座標(Windowsに任せる)
 		CW_USEDEFAULT,         //　表示Y座標(Windowsに任せる)
